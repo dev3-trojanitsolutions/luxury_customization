c7308089faec12ddc028108eea14965054c34cce
dev3-trojanitsolutions <dev3@trojanitsolutions.com>
2026-07-21 18:31:19 +0530

    added missing fields

--- luxury_customization/customizations/company.py
+++ luxury_customization/customizations/company.py
@@ -11,31 +11,37 @@ def create_custom_fields():
                 "insert_after": "default_holiday_list",
                 "reqd": 0
             },
+            {
+                "fieldname": "notify_days",
+                "fieldtype": "Int",
+                "label": "Notify Days",
+                "insert_after": "company_logos",
+                "reqd": 0
+            },
+            {
+                "fieldname": "notify_userd",
+                "fieldtype": "Table MultiSelect",
+                "label": "Notify Users",
+                "insert_after": "company_logos",
+                "options": "Cheque Tracker Settings User",
+                "reqd": 0
+            },
         ]
     }
 
-    for doctype, fields in custom_fields.items():
-        for field in fields:
+    for doctype, fields in custom_fields.items(): 
+        for field in fields: 
             if not frappe.db.exists("Custom Field", {"dt": doctype, "fieldname": field["fieldname"]}):
-                create_custom_field(doctype, field)
-                frappe.db.commit()
+                create_custom_field(doctype, field) 
+                frappe.db.commit() 
                 frappe.clear_cache(doctype=doctype)
 
-def delete_custom_fields():
-    custom_fields_to_delete = { "Company": ["company_logos", "notify_days", "notify_userd"] }
+def delete_custom_fields(): 
+    custom_fields_to_delete = { "Company": ["company_logos", "notify_days", "notify_userd"] }  
 
-    for doctype, fields in custom_fields_to_delete.items():
-        for field_name in fields:
-            if frappe.db.exists("Custom Field", {"dt": doctype, "fieldname": field_name}):
-                frappe.delete_doc("Custom Field", f"{doctype}-{field_name}", ignore_missing=True)
-                frappe.db.commit()
-                frappe.clear_cache(doctype=doctype)
-
-def cleanup_company_document_fields():
-	"""Remove notify_days and notify_userd custom fields from Company Documents if they exist."""
-	fields_to_remove = ["notify_days", "notify_userd"]
-	for field_name in fields_to_remove:
-		if frappe.db.exists("Custom Field", {"dt": "Company Documents", "fieldname": field_name}):
-			frappe.delete_doc("Custom Field", f"Company Documents-{field_name}", ignore_missing=True)
-			frappe.db.commit()
-	frappe.clear_cache(doctype="Company Documents")      
+    for doctype, fields in custom_fields_to_delete.items(): 
+        for field_name in fields: 
+            if frappe.db.exists("Custom Field", {"dt": doctype, "fieldname": field_name}): 
+                frappe.delete_doc("Custom Field", f"{doctype}-{field_name}", ignore_missing=True) 
+                frappe.db.commit() 
+                frappe.clear_cache(doctype=doctype)      
