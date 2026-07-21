65f7b6f469ea6fa846ea9d26532ce80283e29e2b
dev3-trojanitsolutions <dev3@trojanitsolutions.com>
2026-07-21 18:54:00 +0530

    added status field

--- luxury_customization/luxury_customization/doctype/company_documents/company_documents.py
+++ luxury_customization/luxury_customization/doctype/company_documents/company_documents.py
@@ -1,9 +1,14 @@
 # Copyright (c) 2026, Trojan IT Solutions and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe.utils import getdate
 
 
 class CompanyDocuments(Document):
-	pass
+	def before_save(self):
+		if self.expiry_date and getdate(self.expiry_date) < getdate():
+			self.status = "Expired"
+		elif self.expiry_date:
+			self.status = "Active"
