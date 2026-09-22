c95ef02d8e66482566df81d5291f86f6601a6dfb
dev3-trojanitsolutions <dev3@trojanitsolutions.com>
2026-09-22 16:25:37 +0530

    Revert "Employee dropdown: render as two lines (ID / Name) on employee-checkin/new"
    This reverts commit 6bdf8b03f52c762eed4e4d636447e0b90cc79879.

--- luxury_customization/api/employee_checkin.py
+++ luxury_customization/api/employee_checkin.py
@@ -1,28 +1,8 @@
 import frappe
-from frappe.desk.reportview import get_filters_cond, get_match_cond
 
 
 @frappe.whitelist(allow_guest=True)
 def get_employee_details(employee):
 	return frappe.db.get_value(
 		"Employee", employee, ["employee_name", "employee_number"], as_dict=True
-	)
-
-
-@frappe.whitelist(allow_guest=True)
-@frappe.validate_and_sanitize_search_inputs
-def employee_query(doctype, txt, searchfield, start, page_len, filters, **kwargs):
-	doctype = "Employee"
-	conditions = []
-	return frappe.db.sql(
-		"""select name, employee_name
-		from `tabEmployee`
-		where (name like %(txt)s or employee_name like %(txt)s)
-			{fcond} {mcond}
-		order by idx desc, name
-		limit %(page_len)s offset %(start)s""".format(
-			fcond=get_filters_cond(doctype, filters, conditions),
-			mcond=get_match_cond(doctype),
-		),
-		{"txt": f"%{txt}%", "start": start, "page_len": page_len},
 	)
